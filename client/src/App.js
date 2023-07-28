3ba54c2b3fa934a8d7c1c53b31c601c660230182
developerdesigner18 <developer.designer18@gmail.com>
2023-07-28 12:05:11 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from 'axios'
+function App() {
+  const handleTwitterSignIn = async () => {
+    try {
+      // Backend server URL for handling the OAuth callback
+      const backendUrl = "http://localhost:3001";
+
+      // Initiate the OAuth 1.0a flow by making a request to the backend
+      const response = await axios.get(`${backendUrl}/twitter/request-token`);
+
+      // Redirect the user to the Twitter authorization URL
+      window.location.href = response.data.authUrl;
+    } catch (error) {
+      console.error("Error initiating Twitter sign-in:", error);
+    }
+  };
+  return (
+    <div>
+      <h1>Get started, sign in with your Twitter account:</h1>
+      <div style={{ marginTop: 10, marginBottom: 10 }}>
+        <button
+          id="btnSignIn"
+          onClick={handleTwitterSignIn}
+           >
+          {" "}
+          Sign in with Twitter
+        </button>
+        <div
+          id="loggedin"
+          style={{ marginTop: 5, color: "#1E7EBE", fontSize: 10 }}
+        >
+          (You will sign in with the Twitter account your browser is{" "}
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            style={{ textDecoration: "underline" }}
+          >
+            currently logged in
+          </a>{" "}
+          to.)
+        </div>
+        <div style={{ marginTop: 5, color: "#1E7EBE", fontSize: 11 }}>
+          By signing in you agree to our{" "}
+          <a
+            href="/content/terms.php"
+            style={{ color: "#1E7EBE" }}
+            target="_blank"
+          >
+            terms of service
+          </a>{" "}
+          and{" "}
+          <a
+            href="/content/privacy.php"
+            style={{ color: "#1E7EBE" }}
+            target="_blank"
+          >
+            privacy policy
+          </a>
+        </div>
+        <div class="alert alert-error" style={{ width: 46, marginTop: 30 }}>
+          <div style={{ marginBottom: 5 }}>
+            <a
+              class="label label-info"
+              href="https://status.twitter.com/"
+              target="_blank"
+            >
+              Twitter is currently experiencing issues
+            </a>
+          </div>
+          <div>
+            If you cannot sign in to your Twitonomy account or if it does not
+            work properly, please try again later...
+          </div>
+        </div>
+        <div style={{ marginTop: 40 }}>&nbsp;</div>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const app = express();
+const OAuth = require('oauth-1.0a');
+const axios = require('axios');
+const crypto = require('crypto');
+const cors = require('cors')
+app.use(cors())
+const consumerKey = 'wf2dYCRq9PFaDI13bTiFROFz4';
+const consumerSecret = 'NGN9xlVRvFWC6VqwaSREpN1GiXIFa6eSC16SscAsO68oIAAC8n';
+
+const oauth = OAuth({
+  consumer: {
+    key: consumerKey,
+    secret: consumerSecret,
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function(base_string, key) {
+    return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+  },
+});
+
+const getRequestToken = async () => {
+  const requestData = {
+    url: 'https://api.twitter.com/oauth/request_token',
+    method: 'POST',
+    data: { oauth_callback: 'http://localhost:3001/twitter/callback' },
+  };
+
+  try {
+    const response = await axios.post(requestData.url, null, {
+      headers: oauth.toHeader(oauth.authorize(requestData)),
+    });
+
+    const responseData = new URLSearchParams(response.data);
+    const oauthToken = responseData.get('oauth_token');
+    const oauthTokenSecret = responseData.get('oauth_token_secret');
+    console.log('oauthToken:', oauthToken);
+    console.log('oauthTokenSecret:', oauthTokenSecret);
+    return { oauthToken, oauthTokenSecret };
+  } catch (error) {
+    console.error('Error getting request token:', error.message);
+    throw new Error('Failed to get request token.');
+  }
+};
+
+const getAccessToken = async (oauthToken, oauthVerifier) => {
+  const requestData = {
+    url: 'https://api.twitter.com/oauth/access_token',
+    method: 'POST',
+    data: { oauth_verifier: oauthVerifier }, // Change the key to oauth_verifier
+    options: {
+      oauth_token: oauthToken,
+    },
+  };
+
+  try {
+    const headers = oauth.toHeader(
+      oauth.authorize(requestData, { key: oauthToken }) // Add the 'key' option to specify the token
+    );
+
+    const response = await axios.post(requestData.url, null, {
+      headers,
+    });
+
+    const responseData = new URLSearchParams(response.data);
+    const accessToken = responseData.get('oauth_token');
+    const accessTokenSecret = responseData.get('oauth_token_secret');
+
+    return { accessToken, accessTokenSecret };
+  } catch (error) {
+    console.error('Error getting access token:', error.message);
+    console.error('Twitter API error response:', error.response?.data);
+    throw new Error('Failed to get access token.');
+  }
+};
+
+
+// Route to initiate the OAuth 1.0a flow and get the request token
+app.get('/twitter/request-token', async (req, res) => {
+  try {
+    const { oauthToken } = await getRequestToken();
+
+    // Modify the scope parameter in the authorization URL
+    const authUrl = `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`;
+
+// In this example, the application is requesting both read and write access.
+
+    res.json({ authUrl });
+  } catch (error) {
+    console.error('Error during OAuth flow:', error.message);
+    res.status(500).json({ error: 'Failed to initiate OAuth flow.' });
+  }
+});
+
+// Callback route after user authorizes the app
+app.get('/twitter/callback', async (req, res) => {
+  // Extract oauth_verifier and oauth_token from the query parameters
+  const oauthVerifier = req.query.oauth_verifier;
+  const oauthToken = req.query.oauth_token;
+  console.log('oauthVerifier:', oauthVerifier);
+  console.log('oauthToken:', oauthToken);
+
+  try {
+    const { accessToken, accessTokenSecret } = await getAccessToken(
+      oauthToken,
+      oauthVerifier
+    );
+
+    console.log('accessToken:', accessToken);
+    console.log('accessTokenSecret:', accessTokenSecret);
+    const userDetails = await fetchUserDetails(accessToken, accessTokenSecret);
+
+    // At this point, you have the user's access token, access token secret,
+    // and the user details. You can use this information as needed.
+
+    // For demonstration purposes, we'll return the user details as JSON.
+    console.log(userDetails, "sd");
+    res.json(userDetails);
+  } catch (error) {
+    console.error('Error during access token retrieval:', error.message);
+    console.error('Twitter API error response:', error.response?.data);
+    res.status(500).json({ error: 'Failed to get access token.' });
+  }
+});
+
+// const fetchUserDetails = async (accessToken, accessTokenSecret) => {
+//   const client = new Twitter({
+//     consumer_key: consumerKey,
+//     consumer_secret: consumerSecret,
+//     access_token_key: accessToken,
+//     access_token_secret: accessTokenSecret,
+//   });
+
+//   return new Promise((resolve, reject) => {
+//     client.get('account/verify_credentials', {}, (error, user, response) => {
+//       if (error) {
+//         console.error('Error fetching user details:', error.message);
+//         reject(error);
+//       } else {
+//         resolve(user);
+//       }
+//     });
+//   });
+// };
+const fetchUserDetails = async (accessToken, accessTokenSecret) => {
+  const requestData = {
+    url: 'https://api.twitter.com/1.1/account/verify_credentials.json',
+    method: 'GET',
+  };
+
+  try {
+    const headers = oauth.toHeader(
+      oauth.authorize(requestData, {
+        key: accessToken,
+        secret: accessTokenSecret,
+      })
+    );
+
+    const response = await axios.get(requestData.url, {
+      headers,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user details:', error.message);
+    throw new Error('Failed to fetch user details.');
+  }
+};
+
+
+app.listen(3001, () => console.log('Backend server running on port 3001.'));
